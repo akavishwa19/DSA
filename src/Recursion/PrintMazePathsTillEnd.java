20d9452c77368bf8622f34551efebad94fb3034f
Akash <akash19102001@gmail.com>
2025-06-22 03:52:51 +0530

    return paths into list

--- src/Recursion/PrintMazePathsTillEnd.java
+++ src/Recursion/PrintMazePathsTillEnd.java
@@ -1,5 +1,7 @@
 package Recursion;
 
+import java.util.ArrayList;
+
 public class PrintMazePathsTillEnd {
 
     static void printPaths(String unprocessed , int r , int c){
@@ -21,7 +23,35 @@ public class PrintMazePathsTillEnd {
         }
 
     }
+
+    static ArrayList<String> returnPaths(String unprocessed , int r , int c){
+
+        //we get our answer if both row and colum are at the end index
+        if(r==1 && c==1){
+            ArrayList<String> list= new ArrayList<>();
+            list.add(unprocessed);
+            return list;
+        }
+
+        ArrayList<String> answerList=new ArrayList<>();
+
+        //if colum is greater than 1 , that means that we are at the last row , so , we can move right
+        if( c>1 ){
+            answerList.addAll(returnPaths(unprocessed+"R",r,c-1));
+        }
+
+        //if row is greater than 1 , that means we are at the last column , so  , we can move down
+        if( r>1){
+            answerList.addAll(returnPaths(unprocessed+"D",r-1,c));
+        }
+
+        return answerList;
+
+    }
+
+
     public static void main(String[] args) {
-        printPaths("",3,3);
+        ArrayList<String> list = returnPaths("",3,3);
+        System.out.println(list);
     }
 }
