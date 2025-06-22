5ce7e92ee6a66509a1ef286eddaf1821cee3bef5
Akash <akash19102001@gmail.com>
2025-06-22 23:23:03 +0530

    backtracking intro

--- src/Recursion/PrintAllPathsAndMatrix.java
+++ src/Recursion/PrintAllPathsAndMatrix.java
@@ -4,10 +4,18 @@ import java.util.Arrays;
 
 public class PrintAllPathsAndMatrix {
 
+    //take a look at the argument , we have the boolean maze wth those cells being true which could be a possible traversal for the solution'
+    //an unprocessed string which is the path
+    //the representation of the paths traversed by maze in the same order
+    //the step number for the path in the matrix
+    //row and column for traversal
     static void printPaths(String unprocessed,boolean[][] maze,int[][] mazeRep , int r,int c,int step){
 
+        //the particular cell should be the step number
         mazeRep[r][c]=step;
 
+        //base condition
+        //if we reach the end point , simply print the maze representation and the path string
         if(r==maze.length-1 && c==maze[0].length-1 ){
             for(int[] nums:mazeRep){
                 System.out.println(Arrays.toString(nums));
@@ -20,12 +28,13 @@ public class PrintAllPathsAndMatrix {
             printPaths(unprocessed+"R",maze,mazeRep,r,c+1,step+1);
         }
 
+        //if we have more rows , move down
         if(r < maze.length-1){
             printPaths(unprocessed+"D",maze,mazeRep,r+1,c,step+1);
         }
 
+        //when the funtion call returns , change the step representation to 0 in the maze representation cosisting of all the paths
         mazeRep[r][c]=0;
-
 
     }
     public static void main(String[] args) {
